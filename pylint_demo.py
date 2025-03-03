b825870efba928f97a74c6efa311d9c94f3e1fba
MatthiasKebede <msk9862@nyu.edu>
2025-03-03 15:23:11 +0400

    Fix for build

--- pylint_demo.py
+++ pylint_demo.py
@@ -1,10 +1,13 @@
-def sum(a, b):
+"""Demo Module"""
+def my_sum(a, b):
+    """Sum func"""
     return a + b
 
 
 def hello(name):
+    """Hello func"""
     print(f"Hello, {name}!")
 
 
 hello("Alice")
-hello()
+hello("Bob")
